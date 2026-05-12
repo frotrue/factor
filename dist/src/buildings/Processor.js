0180baa58b0587de2a1689a37bda56396cceebd0
user <moojin0830@gmail.com>
2026-05-12 12:40:42 +0900

    chore: add .gitignore and stop tracking node_modules and IDE settings

--- dist/src/buildings/Processor.js
+++
@@ -1,7 +0,0 @@
-import AbstractProcessor from './AbstractProcessor';
-export default class Processor extends AbstractProcessor {
-    constructor(scene, x, y, config = {}) {
-        super(scene, x, y, 'PROCESSOR', 'LABELLING', config);
-    }
-}
-//# sourceMappingURL=Processor.js.map
